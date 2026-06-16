362301d2bf427ca33e64b910d0fb53669be526d4
Avadhi Jain <avadhijain4605@gmail.com>
2026-06-16 22:28:23 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+       priority_queue<int>pq;
+       for(int i:stones){
+        pq.push(i);
+       } 
+       while(pq.size()>=2){
+        int x=pq.top();
+        pq.pop();
+        int y=pq.top();
+        pq.pop();
+        if(x==y)continue;
+        else {
+            pq.push(x-y);
+        }
+       }
+       if(pq.size()==1) return pq.top();
+       return 0;
+    }
+};
