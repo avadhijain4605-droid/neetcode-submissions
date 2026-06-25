cd022af10e83ef22f991272ffa49e271b155f1bc
Avadhi Jain <avadhijain4605@gmail.com>
2026-06-25 15:31:55 +0530

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int bfs(int i,int j,vector<vector<int>>& grid,vector<vector<bool>>& vis){
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        vis[i][j]=true;
+        vector<vector<int>>dir={{0,1},{0,-1},{1,0},{-1,0}};
+        int area=0;
+        while(!q.empty()){
+            int x=q.front().first;
+            int y=q.front().second;
+            area++;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int dx=x+dir[i][0];
+                int dy=y+dir[i][1];
+                if(dx>=0&&dx<n&&dy>=0&&dy<m&&grid[dx][dy]==1&&!vis[dx][dy]){
+                    vis[dx][dy]=true;
+                    q.push({dx,dy});
+                }
+            }
+        }
+        return area;
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        int maxArea=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1&& !vis[i][j]){
+                    maxArea=max(maxArea,bfs(i,j,grid,vis));
+                }
+            }
+        }
+        return maxArea;
+    }
+};
