27e2b5ce7ceae81f8d2c74d467c4d5509d5c6f48
Avadhi Jain <avadhijain4605@gmail.com>
2026-06-28 19:57:01 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1||n==2) return n;
+        int prev2=1;
+        int prev=2;
+        int curr;
+        for(int i=3;i<=n;i++){
+            curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
+        }
+        return curr;
+    }
+};
